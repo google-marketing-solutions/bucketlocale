704aa5f1364cdf39dd91df4a105c0fed86424b64
Eylon Goor <eylong@google.com>
2025-11-05 17:08:57 +0200

    add license and headers
    Change-Id: I5cfc061a9ab45b7cb110e21c32db82c2b9f7a2ec

--- src/stores/config.ts
+++ src/stores/config.ts
@@ -1,3 +1,19 @@
+/*
+ Copyright 2025 Google LLC
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+      https://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+*/
+
 import { reactive } from 'vue';
 
 const CONFIG_STORAGE_KEY = 'bucketlocale_config';
@@ -18,7 +34,7 @@ export interface Config {
 
 // Default configuration
 const defaultConfig: Config = {
-  model: 'gemini-2.5-flash',
+  model: 'gemini-1.5-flash',
   geminiApiKey: '',
   googleClientId: '',
   googleAdsDeveloperToken: '',
